48f7d6127a37231a94b0731e00f4aefe8da3dfc7
siyrs <95621941@qq.com>
2025-06-08 20:41:36 +0800

    add logo

--- NANA.sol
+++ NANA.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @title NANA Meme Token
 /// @notice NANA is a vibrant anime-themed ERC-20 meme token on the Ethereum blockchain, inspired by the character Shana from “Shakugan no Shana”. The total supply is 88 billion tokens, with 8 billion reserved by the team for operations. NANA is designed as a community-driven project – the contract is standard and fee-free (0% transfer tax), and ownership will be renounced after launch for transparency. Early holders can enjoy the fantasy-inspired branding and participate in future community events. All tokenomics and metadata will be published openly, and the project welcomes anime & crypto fans to join the adventure.
+
 contract NANA is ERC20, ERC20Burnable {
     // 总发行量：880,000,000,000 × 10^18
     uint256 public constant INITIAL_SUPPLY = 880_000_000_000 * (10 ** 18);
